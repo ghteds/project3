8727f1700fea7bfd1dd9cd200cfe39942e1b97c2
BCstag <tstagner@bellsouth.net>
2019-01-09 20:33:51 -0500

    flask

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect
 
+import pandas as pd
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+#from flask.ext.mysqldb import MySQL
 
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -15,7 +17,15 @@ engine = create_engine(f'mysql://{connection_string}')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{connection_string}'
+
+Anemia = Base.classes.anem_tbl
+Depression = Base.classes.depr_tbl
+   
+db=SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
@@ -24,11 +34,49 @@ def index():
 
 @app.route('/anemia')
 def anemia():
-#print(Base.classes.keys())
-    result = engine.execute('select * from anem_tbl').fetchall()
-    #print (result)
-    return jsonify(result)
+    result=db.session.query(Anemia).statement
 
+    df = pd.read_sql_query(result, db.session.bind)
+
+    data = {
+        'FacilityName': df['FacilityName'].tolist(),
+        'CCN':df['CCN'].tolist(),
+        'City':df['City'].tolist(),
+        'State':df['State'].tolist(),
+        'Zipcode':df['Zipcode'].tolist(),
+        'AnemiaScore':df['AnemiaScore'].tolist(),
+        'StateAvg':df['StateAvg'].tolist(),
+        'NationalAvg':df['NationalAvg'].tolist(),
+        'ZipLat':df['ZipLat'].tolist(),
+        'ZipLon':df['ZipLon'].tolist(),
+        'StateLat':df['StateLat'].tolist(),
+        'StateLon':df['StateLon'].tolist(),
+    }
+    
+    return  jsonify(data)
+
+@app.route('/depression')
+def depression():
+    result=db.session.query(Depression).statement
+
+    df = pd.read_sql_query(result, db.session.bind)
+
+    data = {
+        'FacilityName': df['FacilityName'].tolist(),
+        'CCN':df['CCN'].tolist(),
+        'City':df['City'].tolist(),
+        'State':df['State'].tolist(),
+        'Zipcode':df['Zipcode'].tolist(),
+        'DepressionScore':df['DepressionScore'].tolist(),
+        'StateAvg':df['StateAvg'].tolist(),
+        'NationalAvg':df['NationalAvg'].tolist(),
+        'ZipLat':df['ZipLat'].tolist(),
+        'ZipLon':df['ZipLon'].tolist(),
+        'StateLat':df['StateLat'].tolist(),
+        'StateLon':df['StateLon'].tolist(),
+    }
+    
+    return  jsonify(data)
 
 
 if __name__ == "__main__":
